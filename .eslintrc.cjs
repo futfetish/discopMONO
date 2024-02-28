5a1562d6ee5a912292358b8d0f50fd2210eb3f14
futfetish <gr1zzly.medvegonok@gmail.com>
2024-02-28 17:34:06 +0300

    import type eslint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,14 +21,7 @@ const config = {
     '@next/next/no-page-custom-font' : 'off' ,
     "@typescript-eslint/no-unsafe-return " : 'off',
     "@typescript-eslint/no-unsafe-return" : 'off',
-
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
+    '@typescript-eslint/consistent-type-imports' : 'off',
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": [
       2,
