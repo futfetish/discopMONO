65b28215cfe3785c9a03024c8df977e60e672e1a
futfetish <gr1zzly.medvegonok@gmail.com>
2024-09-23 11:39:01 +0300

    unreadrooms panel integration

--- web/src/modules/layout/features/unreadRooms/unreadRooms.tsx
+++ web/src/modules/layout/features/unreadRooms/unreadRooms.tsx
@@ -1,13 +1,14 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { socket } from "~/socket";
 import { roomType } from "~/types/rooms";
 import { api } from "~/utils/api";
 import Styles from "./unreadRooms.module.scss";
 import Link from "next/link";
 import { useAppSelector } from "~/hooks/redux";
+import { Panel } from "~/modules/common/ui/panel/panel";
 
 export const UnReadRooms: FC = () => {
-  const user = useAppSelector(state => state.global.user)
+  const user = useAppSelector((state) => state.global.user);
   const { data: unReadRoomsQ } = api.rooms.unReadRooms.useQuery();
   const [unReadRooms, setUnReadRooms] = useState<roomType[]>([]);
   const { mutate: addRoomToUnread } = api.rooms.getById.useMutation({
@@ -43,28 +44,26 @@ export const UnReadRooms: FC = () => {
   }
 
   return (
-    <div className={Styles.unread_rooms_container}>
-      <div className={Styles.unread_rooms}>
-        {unReadRooms.map((room) => (
-          <UnReadRoomItem
-            key={room.id}
-            room={room}
-            onClick={() => deleteFromUnReadList(room.id)}
-            image={
-              room.type == "group"
-                ? "/img/grav.png"
-                : room.members.map((u) => u.user).find((m) => m.id !== user.id)
-                    ?.image || ''
-            }
-            name={
-              (room.type == "group"
-                ? room.name
-                : room.members.map((u) => u.user).find((m) => m.id !== user.id)
-                    ?.name) || '[blank]'
-            }
-          />
-        ))}
-      </div>
+    <div className={`${Styles.unread_rooms} hidden_scroll`}>
+      {unReadRooms.map((room) => (
+        <UnReadRoomItem
+          key={room.id}
+          room={room}
+          onClick={() => deleteFromUnReadList(room.id)}
+          image={
+            room.type == "group"
+              ? "/img/grav.png"
+              : room.members.map((u) => u.user).find((m) => m.id !== user.id)
+                  ?.image || ""
+          }
+          name={
+            (room.type == "group"
+              ? room.name
+              : room.members.map((u) => u.user).find((m) => m.id !== user.id)
+                  ?.name) || "[blank]"
+          }
+        />
+      ))}
     </div>
   );
 };
@@ -75,9 +74,17 @@ const UnReadRoomItem: FC<{
   image: string;
   name: string;
 }> = ({ room, onClick, image, name }) => {
+
+  const parentRef = useRef<HTMLDivElement>(null);
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+
   return (
-    <div key={room.id} className={Styles.item}>
-      <div className={Styles.item__content}>
+    <div key={room.id} className={Styles.item}  ref={parentRef} >
+      <div
+        className={Styles.item__content}
+        onMouseEnter={() => setIsPanelOpen(true)}
+        onMouseLeave={() => setIsPanelOpen(false)}
+      >
         <Link href={"/channels/" + room.id} onClick={() => onClick()}>
           <img alt="" src={image} />
         </Link>
@@ -87,12 +94,23 @@ const UnReadRoomItem: FC<{
       </div>
 
       <div className={Styles.item__white_stick}></div>
-      <div className={Styles.item_info_container}>
-        <div className={Styles.arrow}></div>
-        <div className={Styles.item__info}>
-          <p>{name}</p>
+      <Panel
+      scaleAnimation={{startScale : 20 , duration : 200}}
+      opacityAnimation={{duration : 200}}
+      yCenter={true}
+        useLeft={true}
+        offsetPercentage={{ left: 100 }}
+        isOpen={isPanelOpen}
+        setIsOpen={setIsPanelOpen}
+        parentRef={parentRef}
+      >
+        <div className={Styles.item_info_container}>
+          <div className={Styles.arrow}></div>
+          <div className={Styles.item__info}>
+            <p>{name}</p>
+          </div>
         </div>
-      </div>
+      </Panel>
     </div>
   );
 };
